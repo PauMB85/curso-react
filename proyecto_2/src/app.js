f3a817f8722167754c3ee9eb54c49fee7417bdf9
PauMB <paumb85@gmail.com>
2019-03-26 16:32:52 +0100

    listado de componentes

--- proyecto_2/src/app.js
+++ proyecto_2/src/app.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import Saludar from './components/Saludar';
 import SaludarFunc from './components/SaludarFunc';
 import Contador from './components/Contador'
+import Mascota from './components/Mascota';
 
 const App = (props) => {
+    const mascotas = ['perro', 'gato', 'canario'];
+
+    //react no tiene un ngFor como en angular, hay que utilizar el map de js
+
+    const cmpMascotas = mascotas.map((m, pos) => 
+        <Mascota key={pos} mascota={m} />
+    );
     return (
         <div>
             <h1>Hola Mundo!</h1>
             <p>Me llamo Pau</p>
+            {cmpMascotas}
             <Saludar nombre="Pau"/>
             <Saludar nombre="Tomeu"/>
             <Saludar nombre="Quely"/>
---
+++ proyecto_2/src/components/Mascota.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Mascota = props => {
+    return (
+        <p>{props.mascota}</p>
+    )
+}
+
+export default Mascota;
